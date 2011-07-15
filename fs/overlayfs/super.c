e10a4787c099d6f265ba0b1494af6ec5310eb5f4
Miklos Szeredi <mszeredi@suse.cz>
2011-07-15 14:49:20 +0200

    UBUNTU: ubuntu: overlayfs -- ovl: improve stack use of lookup and readdir
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -248,7 +248,7 @@ static struct ovl_entry *ovl_alloc_entry(void)
 	return kzalloc(sizeof(struct ovl_entry), GFP_KERNEL);
 }
 
-static struct dentry *ovl_lookup_real(struct dentry *dir, struct qstr *name)
+static inline struct dentry *ovl_lookup_real(struct dentry *dir, struct qstr *name)
 {
 	struct dentry *dentry;
 
@@ -266,7 +266,7 @@ static struct dentry *ovl_lookup_real(struct dentry *dir, struct qstr *name)
 	return dentry;
 }
 
-int ovl_do_lookup(struct dentry *dentry)
+static int ovl_do_lookup(struct dentry *dentry)
 {
 	struct ovl_entry *oe;
 	struct dentry *upperdir;
@@ -361,6 +361,17 @@ out_put_dir:
 	kfree(oe);
 out:
 	return err;
+}
+
+struct dentry *ovl_lookup(struct inode *dir, struct dentry *dentry,
+			  struct nameidata *nd)
+{
+	int err = ovl_do_lookup(dentry);
+
+	if (err)
+		return ERR_PTR(err);
+
+	return NULL;
 }
 
 static void ovl_put_super(struct super_block *sb)
