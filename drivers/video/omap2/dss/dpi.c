e620b8b83092be5da89443c0e0c5ffe20e77e819
Leann Ogasawara <leann.ogasawara@canonical.com>
2011-08-18 07:47:51 -0700

    Revert "UBUNTU: SAUCE: OMAP: DSS2: enable hsclk in dsi_pll_init for OMAP36XX"
    This reverts commit d360158cc2b48727a2215ebc1e78c3b1d171335e.
    
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- drivers/video/omap2/dss/dpi.c
+++ drivers/video/omap2/dss/dpi.c
@@ -203,10 +203,7 @@ int omapdss_dpi_display_enable(struct omap_dss_device *dssdev)
 
 	if (dpi_use_dsi_pll(dssdev)) {
 		dss_clk_enable(DSS_CLK_SYSCK);
-		if (cpu_is_omap3630())
-			r = dsi_pll_init(dssdev, 1, 1);
-		else
-			r = dsi_pll_init(dssdev, 0, 1);
+		r = dsi_pll_init(dpi.dsidev, 0, 1);
 		if (r)
 			goto err3;
 	}
