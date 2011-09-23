a69aa870def13f1c9a052520033f4934a37639c0
Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>
2011-09-23 18:31:22 +0100

    OMAP4: Pandaboard: Fixing Leds for 4460
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -99,12 +99,10 @@ static struct gpio_led gpio_leds[] = {
 	{
 		.name			= "pandaboard::status1",
 		.default_trigger	= "heartbeat",
-		.gpio			= 7,
 	},
 	{
 		.name			= "pandaboard::status2",
 		.default_trigger	= "mmc0",
-		.gpio			= 8,
 	},
 };
 
@@ -142,8 +140,20 @@ static struct platform_device omap4panda_hdmi_audio_device = {
 	.resource = omap4panda_hdmi_resources,
 };
 
+static void __init panda_leds_init(void)
+{
+	if (cpu_is_omap443x()) {
+		gpio_leds[0].gpio = 7;
+		gpio_leds[1].gpio = 8;
+	} else {
+		gpio_leds[0].gpio = 110;
+		gpio_leds[1].gpio = 8;
+	}
+
+	platform_device_register(&leds_gpio);
+}
+
 static struct platform_device *panda_devices[] __initdata = {
-	&leds_gpio,
 	&wl1271_device,
 	&btwilink_device,
 	&omap4panda_hdmi_audio_device,
@@ -721,6 +731,7 @@ static void __init omap4_panda_init(void)
 	omap4_panda_i2c_init();
 
 	platform_add_devices(panda_devices, ARRAY_SIZE(panda_devices));
+	panda_leds_init();
 	platform_device_register(&omap_vwlan_device);
 	board_serial_init();
 	omap4_twl6030_hsmmc_init(mmc);
