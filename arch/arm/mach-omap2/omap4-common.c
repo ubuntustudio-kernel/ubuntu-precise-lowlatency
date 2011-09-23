671dc677d3ca1c5ec81947d7a91362d7eb03f2ad
Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>
2011-09-23 18:31:28 +0100

    OMAP4460: L2X0 workaround applies just at 4460 ES1.0
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -145,7 +145,7 @@ static int __init omap_l2_cache_init(void)
 	 * Lock-down specific L2 cache ways which  makes effective
 	 * L2 size as 512 KB instead of 1 MB
 	 */
-	if (cpu_is_omap446x()) {
+	if (omap_rev() == OMAP4460_REV_ES1_0) {
 		lockdown = 0xa5a5;
 		writel_relaxed(lockdown, l2cache_base + L2X0_LOCKDOWN_WAY_D0);
 		writel_relaxed(lockdown, l2cache_base + L2X0_LOCKDOWN_WAY_D1);
