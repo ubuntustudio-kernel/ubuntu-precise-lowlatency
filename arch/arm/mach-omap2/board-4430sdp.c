2feeed19368054434fe8730240c3d1ab32fcaed1
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-12-12 17:00:56 +0800

    OMAP4: 4430SDP: Add panel support to board file
    4430SDP has two Taal DSI panels, connected to DSI 1 and DSI 2 modules.
    The panels use a common PWM backlight, which will be implemented later
    when the PWM driver has been improved to support the backlight.
    
    Until the PWM driver has been improved, the following hack added to
    arch/arm/mach-omap2/board-4430sdp.c can be used to set the backlight to
    max:
    
    static int omap_4430sdp_hack_backlight(void)
    {
           twl_i2c_write_u8(TWL_MODULE_PWM, 0x7f, LED_PWM2OFF);
           twl_i2c_write_u8(TWL_MODULE_PWM, 0x7f, LED_PWM2ON);
           twl_i2c_write_u8(TWL6030_MODULE_ID1, 0x30, TWL6030_TOGGLE3);
           return 0;
    }
    late_initcall(omap_4430sdp_hack_backlight);
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -56,8 +56,8 @@
 #define HDMI_GPIO_CT_CP_HPD 60 /* HPD mode enable/disable */
 #define HDMI_GPIO_LS_OE 41 /* Level shifter for HDMI */
 #define HDMI_GPIO_HPD  63 /* Hotplug detect */
-#define DISPLAY_SEL_GPIO	59	/* LCD2/PicoDLP switch */
 #define DLP_POWER_ON_GPIO	40
+#define DISPLAY_SEL_GPIO	59	/* LCD2/PicoDLP switch */
 
 #define GPIO_WIFI_PMENA		54
 #define GPIO_WIFI_IRQ		53
