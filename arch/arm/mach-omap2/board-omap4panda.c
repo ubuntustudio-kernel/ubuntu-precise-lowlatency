f1bd2c9cafd5684593d5c5aeeb10d234932190a2
Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>
2011-12-12 17:00:24 +0800

    omap4: panda: enabling tps6236x
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -57,6 +57,7 @@
 #define HDMI_GPIO_CT_CP_HPD 60 /* HPD mode enable/disable */
 #define HDMI_GPIO_LS_OE 41 /* Level shifter for HDMI */
 #define HDMI_GPIO_HPD  63 /* Hotplug detect */
+#define TPS62361_GPIO   7
 
 static int
 panda_kim_suspend(struct platform_device *pdev, pm_message_t msg)
@@ -579,6 +580,7 @@ void omap4_panda_display_init(void)
 
 static void __init omap4_panda_init(void)
 {
+	int status;
 	int package = OMAP_PACKAGE_CBS;
 
 	if (omap_rev() == OMAP4430_REV_ES1_0)
@@ -597,6 +599,15 @@ static void __init omap4_panda_init(void)
 	omap4_ehci_init();
 	usb_musb_init(&musb_board_data);
 	omap4_panda_display_init();
+
+	if (cpu_is_omap446x()) {
+		/* Vsel0 = gpio, vsel1 = gnd */
+		status = omap_tps6236x_board_setup(true, TPS62361_GPIO, -1,
+				OMAP_PIN_OFF_OUTPUT_HIGH, -1);
+		if (status)
+			pr_err("TPS62361 initialization failed: %d\n", status);
+	}
+
 	omap_enable_smartreflex_on_init();
 }
 
