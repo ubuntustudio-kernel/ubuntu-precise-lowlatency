c613889a56bf830b605d4ba35ec02a55c355490b
Andy Green <andy.green@linaro.org>
2011-12-16 13:05:34 +0800

    hwmod disable fdif
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -5727,7 +5727,7 @@ static __initdata struct omap_hwmod *omap44xx_hwmods[] = {
 	&omap44xx_dss_venc_hwmod,
 
 	/* fdif class */
-	&omap44xx_fdif_hwmod,
+//	&omap44xx_fdif_hwmod,
 
 	/* gpio class */
 	&omap44xx_gpio2_hwmod,
