27351d015db240e7e488a3a2e28cc5e53146dc56
Paolo Pisati <paolo.pisati@canonical.com>
2012-01-25 16:26:51 +0100

    Revert "This commit changes various functions that change pages and"
    This reverts commit bf6c77d9def2e12e89157ac146a0b0898bb6203c.
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- include/linux/page-isolation.h
+++ include/linux/page-isolation.h
@@ -3,7 +3,7 @@
 
 /*
  * Changes migrate type in [start_pfn, end_pfn) to be MIGRATE_ISOLATE.
- * If specified range includes migrate types other than MOVABLE or CMA,
+ * If specified range includes migrate types other than MOVABLE,
  * this will fail with -EBUSY.
  *
  * For isolating all pages in the range finally, the caller have to
@@ -11,31 +11,30 @@
  * test it.
  */
 extern int
-start_isolate_page_range(unsigned long start_pfn, unsigned long end_pfn,
-			 unsigned migratetype);
+start_isolate_page_range(unsigned long start_pfn, unsigned long end_pfn);
 
 /*
  * Changes MIGRATE_ISOLATE to MIGRATE_MOVABLE.
  * target range is [start_pfn, end_pfn)
  */
 extern int
-undo_isolate_page_range(unsigned long start_pfn, unsigned long end_pfn,
-			unsigned migratetype);
+undo_isolate_page_range(unsigned long start_pfn, unsigned long end_pfn);
 
 /*
- * Test all pages in [start_pfn, end_pfn) are isolated or not.
+ * test all pages in [start_pfn, end_pfn)are isolated or not.
  */
-int test_pages_isolated(unsigned long start_pfn, unsigned long end_pfn);
+extern int
+test_pages_isolated(unsigned long start_pfn, unsigned long end_pfn);
 
 /*
- * Internal functions. Changes pageblock's migrate type.
+ * Internal funcs.Changes pageblock's migrate type.
+ * Please use make_pagetype_isolated()/make_pagetype_movable().
  */
-int set_migratetype_isolate(struct page *page);
-void unset_migratetype_isolate(struct page *page, unsigned migratetype);
+extern int set_migratetype_isolate(struct page *page);
+extern void unset_migratetype_isolate(struct page *page);
 
 /* The below functions must be run on a range from a single zone. */
-int alloc_contig_range(unsigned long start, unsigned long end,
-		       unsigned migratetype);
+int alloc_contig_range(unsigned long start, unsigned long end);
 void free_contig_range(unsigned long pfn, unsigned nr_pages);
 
 /* CMA stuff */
