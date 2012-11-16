a7495e1f871b2575d4c93650e9305553d3c9d7ac
Andy Whitcroft <apw@canonical.com>
2012-11-16 00:38:59 +0000

    UBUNTU: [Config] enable lowlatency-pae on i386
    BugLink: http://bugs.launchpad.net/bugs/1079594
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.lowlatency/rules.d/i386.mk
+++ debian.lowlatency/rules.d/i386.mk
@@ -2,7 +2,7 @@
 build_arch	= i386
 header_arch	= x86_64
 defconfig	= defconfig
-flavours	= lowlatency
+flavours	= lowlatency lowlatency-pae
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
