a6256113b47424cad89a4090e7a9c31b7f201e08
Andy Whitcroft <apw@canonical.com>
2012-11-16 00:38:59 +0000

    UBUNTU: [Config] enable lowlatency-pae on i386
    BugLink: http://bugs.launchpad.net/bugs/1079594
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.lowlatency/rules.d/hooks.mk
+++ debian.lowlatency/rules.d/hooks.mk
@@ -7,4 +7,5 @@ hook-before-clean::
 	cp -rp $(CURDIR)/debian.master/d-i/* $(DEBIAN)/d-i
 	mv $(DEBIAN)/d-i/kernel-versions.in-keep $(DEBIAN)/d-i/kernel-versions.in
 	cat $(DEBIAN)/config/i386/config.flavour.generic $(DEBIAN)/config-delta >$(DEBIAN)/config/i386/config.flavour.lowlatency
+	cat $(DEBIAN)/config/i386/config.flavour.generic-pae $(DEBIAN)/config-delta >$(DEBIAN)/config/i386/config.flavour.lowlatency-pae
 	cat $(DEBIAN)/config/amd64/config.flavour.generic $(DEBIAN)/config-delta >$(DEBIAN)/config/amd64/config.flavour.lowlatency
