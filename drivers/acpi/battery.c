22b4f3c332f4d3d454d6c071539edf12a26863eb
Lan Tianyu <tianyu.lan@intel.com>
2014-07-07 15:47:12 +0800

    ACPI / battery: Retry to get battery information if failed during probing
    BugLink: http://bugs.launchpad.net/bugs/1355293
    
    commit 75646e758a0ecbed5024454507d5be5b9ea9dcbf upstream.
    
    Some machines (eg. Lenovo Z480) ECs are not stable during boot up
    and causes battery driver fails to be loaded due to failure of getting
    battery information from EC sometimes. After several retries, the
    operation will work. This patch is to retry to get battery information 5
    times if the first try fails.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=75581
    Reported-and-tested-by: naszar <naszar@ya.ru>
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [bwh: Backported to 3.2: acpi_battery_update() doesn't take a second parameter]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Joseph Salisbury <joseph.salisbury@canonical.com>

--- drivers/acpi/battery.c
+++ drivers/acpi/battery.c
@@ -34,6 +34,7 @@
 #include <linux/dmi.h>
 #include <linux/slab.h>
 #include <linux/suspend.h>
+#include <linux/delay.h>
 #include <asm/unaligned.h>
 
 #ifdef CONFIG_ACPI_PROCFS_POWER
@@ -1053,6 +1054,28 @@ static int battery_notify(struct notifier_block *nb,
 	}
 
 	return 0;
+}
+
+/*
+ * Some machines'(E,G Lenovo Z480) ECs are not stable
+ * during boot up and this causes battery driver fails to be
+ * probed due to failure of getting battery information
+ * from EC sometimes. After several retries, the operation
+ * may work. So add retry code here and 20ms sleep between
+ * every retries.
+ */
+static int acpi_battery_update_retry(struct acpi_battery *battery)
+{
+	int retry, ret;
+
+	for (retry = 5; retry; retry--) {
+		ret = acpi_battery_update(battery);
+		if (!ret)
+			break;
+
+		msleep(20);
+	}
+	return ret;
 }
 
 static LIST_HEAD(acpi_battery_domain);
